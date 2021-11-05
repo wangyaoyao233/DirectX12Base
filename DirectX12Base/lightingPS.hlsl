c2c0425190353a721d594012ed06807343a0ab57
wym <1wangyumeng@gmail.com>
2021-11-05 09:46:33 +0900

    2021_11_05 depth fog

--- DirectX12Base/lightingPS.hlsl
+++ DirectX12Base/lightingPS.hlsl
@@ -20,36 +20,31 @@ struct PS_INPUT
 
 float4 main(PS_INPUT input) : SV_TARGET
 {
+    float4 outDiffuse;
+    
     float4 normal = normalTex.Sample(sampler0, input.TexCoord);
     float4 color = colorTex.Sample(sampler0, input.TexCoord);
     float4 worldPos = worldPosTex.Sample(sampler0, input.TexCoord);
     float4 depth = depthTex.Sample(sampler0, input.TexCoord);
     
-    normal = normalize(normal);
-    
-    float3 lightDir = normalize(float3(1, -1, 1));
-    //float3 lightDir = normalize(LightDirection.xyz);
-    
-    float NDotL = saturate(-dot(normal.xyz, lightDir)); //ランバート拡散
-    
-    float4 outDiffuse;
+    //// ランバート拡散 
+    normal = normalize(normal);   
+    float3 lightDir = normalize(float3(1, -1, 1)); 
+    float NDotL = saturate(-dot(normal.xyz, lightDir));   
     outDiffuse = color * NDotL;
      
     //// specular
     float3 cameraPos = float3(0, 2, -5);
-    //float3 eyev = normalize(input.WorldPostion.xyz - CameraPostion.xyz);
     float3 eyev = normalize(worldPos.xyz - cameraPos);
     float3 refv = normalize(reflect(lightDir, normal.xyz));
     float spec = saturate(-dot(eyev, refv));
     spec = pow(spec, 30);
     outDiffuse.rgb += spec;
       
-    // fog
-    float4 fogColor = float4(0, 0, 0, 1);
-    float4 finalColor;
-    finalColor.rgb = lerp(outDiffuse.rgb, fogColor.rgb, depth.x / 15);
-    finalColor.a = outDiffuse.a;
+    //// depth fog
+    float3 fogColor = float3(0, 0, 0);
+    outDiffuse.rgb = lerp(outDiffuse.rgb, fogColor.rgb, depth.x / 15);
     
     
-    return finalColor;
+    return outDiffuse;
 }
